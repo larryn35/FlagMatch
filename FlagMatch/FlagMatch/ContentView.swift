8d91220afe37d35e4b6f87398e77433808a90dfd
Larry N <larryn35@gmail.com>
2020-09-07 23:26:37 -0700

    add random flag images to view

--- FlagMatch/FlagMatch/ContentView.swift
+++ FlagMatch/FlagMatch/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia","France","Germany","Ireland","Italy","Nigeria","Poland","Spain","UK","US"].shuffled()
+    var correctAnswer = Int.random(in: 0...2)
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack {
+            
+            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 2, endRadius: 800).edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 20) {
+                Text("Tap the flag of")
+                    .foregroundColor(Color.white)
+                Text(countries[correctAnswer])
+                    .foregroundColor(Color.white)
+                
+                VStack(spacing: 20) {
+                    ForEach (0 ..< 3) { number in
+                        Button(action: {
+                        }) {
+                            Image(self.countries[number]).renderingMode(.original)
+                        }
+                    }
+                    Spacer()
+
+                }
+            }
+        }
     }
 }
 
