3703e475510153540ad2f52b79497b53b8a1327c
Larry N <larryn35@gmail.com>
2020-09-08 01:00:46 -0700

    track score, reset score, show correct answer

--- FlagMatch/FlagMatch/ContentView.swift
+++ FlagMatch/FlagMatch/ContentView.swift
@@ -11,7 +11,19 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var showScore = false
+    
+    @State private var score = 0
+    @State private var numberOfQuestions = 0
+    @State private var message = ""
     @State private var scoreTitle = ""
+    
+    var percentCorrect: Double {
+        if score == 0 {
+            return 0
+        } else {
+            return (Double(score) / Double(numberOfQuestions) + 0) * 100
+        }
+    }
     
     @State private var countries = ["Estonia","France","Germany","Ireland","Italy","Nigeria","Poland","Spain","UK","US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -24,7 +36,9 @@ struct ContentView: View {
             RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 2, endRadius: 800)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack(spacing: 20) {
+            VStack(spacing: 40) {
+                
+                Spacer()
                 Text("Tap the flag of")
                     .foregroundColor(Color.white)
                     .font(.title)
@@ -45,13 +59,39 @@ struct ContentView: View {
                                 .shadow(color: .black, radius: 2)
                         }
                     }
+                    
                     Spacer()
                     
+                    Text("Score: \(score) / \(numberOfQuestions) (\(percentCorrect, specifier: "%.0f")%)")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                               
+                    Spacer()
+                    
+                    Button(action: {
+                        self.score = 0
+                        self.numberOfQuestions = 0
+                        self.askQuestions()
+                    }) {
+                        Text("Reset score")
+                            .font(.body)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(40)
+                            .foregroundColor(Color.black)
+                            .padding(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 40)
+                                    .stroke(Color.white, lineWidth: 5))
+                    }
+                    
+                    Spacer()
+
                 }
             }
                 
             .alert(isPresented: $showScore) {
-                Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue")) {
                     self.askQuestions()
                     })
             }
@@ -61,8 +101,13 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            message = "Good job!"
+            score += 1
+            numberOfQuestions += 1
         } else {
             scoreTitle = "Incorrect"
+            message = "That is the flag of \(countries[number])"
+            numberOfQuestions += 1
         }
         
         showScore = true
